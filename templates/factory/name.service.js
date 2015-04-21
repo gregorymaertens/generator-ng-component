8a61a797e10b1614fad2c19e3e759ecaed3744fd
gregory maertens <greg@conjurelabs.com>
2015-04-21 16:14:19 +0200

    minor edits

--- templates/factory/name.service.js
+++ templates/factory/name.service.js
@@ -1,17 +1,14 @@
 (function(){
   'use strict';
 
-  angular.module('<%= scriptAppName %>').factory('<%= cameledName %>'Factory, <%= cameledName %>Factory);
+  angular.module('<%= scriptAppName %>').factory('<%= cameledName %>Factory', <%= cameledName %>Factory);
 
   function <%= cameledName %>Factory() {
 
-    // Public API here
     var factory = {
       someMethod: someMethod
     };
 
-    // Service logic
-    // ...
     var meaningOfLife = 42;
 
     return factory;
